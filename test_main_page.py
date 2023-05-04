a879754641059f52d37fed2e3e367669e37d33dc
Katya Fadeeva <katfadmail@gmail.com>
2023-05-04 13:35:16 +0200

    deleted separate test for loginpage, added loginpage check to mainpage

--- test_main_page.py
+++ test_main_page.py
@@ -14,23 +14,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-
-
-def test_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
-
-
-#def go_to_login_page(browser):
-#    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-#    login_link.click()
-
-
-#def test_add_to_cart(browser):
-#    page = ProductPage(url="", browser)   # инициализируем объект Page Object
-#    page.open()                           # открываем страницу в браузере
-#    page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
-#    page.add_product_to_cart()            # жмем кнопку добавить в корзину 
-#    page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
