8920b1904b3dbd4527bfbfa25542f38c7a2daead
Katya Fadeeva <katfadmail@gmail.com>
2023-05-04 13:26:18 +0200

    test for LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,28 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 # pytest -v --tb=line --language=en test_main_page.py
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+    
 def test_guest_can_go_to_login_page(browser): 
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+
+def test_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_link()
-    
+    page.should_be_login_page()
+
+
 #def go_to_login_page(browser):
 #    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
 #    login_link.click()
