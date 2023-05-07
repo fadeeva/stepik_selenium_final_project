ee298d0cf71b26d2d6ae191ae9d7cf670540736d
Katya Fadeeva <katfadmail@gmail.com>
2023-05-07 15:36:58 +0200

    moved login-page tests from MainPage_class to BasePage_class

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,11 @@
 from selenium.webdriver.common.by import By
 
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
 
@@ -7,6 +13,7 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
 
 class ProductPageLocators():
     ADD_ITEM_BTN = (By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
