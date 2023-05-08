b3ac971022e7221c8f9602689a1e1bd00f6d7250
Katya Fadeeva <katfadmail@gmail.com>
2023-05-08 10:44:52 +0200

    test empty basket from productPage

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 # pytest -v -s --tb=line --language=en test_product_page.py
 @pytest.mark.skip
@@ -74,6 +75,13 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     
 
-@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    pass
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_msg()
+
+
